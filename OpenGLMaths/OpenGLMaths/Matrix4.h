d0c4ac4f8897e473059761f7e3b71408509b11c9
ArthurJacquin <artuj38@free.fr>
2019-07-02 20:54:28 +0200

    MVP matrix added but not fonctionnal + Vec3 class added

--- OpenGLMaths/OpenGLMaths/Matrix4.h
+++ OpenGLMaths/OpenGLMaths/Matrix4.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <memory.h>
-
+#include "Vec3.h"
 
 struct Matrix4
 {
@@ -23,8 +23,15 @@ struct Matrix4
 	Matrix4() {
 		loadIdentity();
 	}
-	Matrix4(const float* data) {
-		memcpy(m, data, sizeof(float) * 16);
+	Matrix4(float m00, float m01, float m02, float m03,
+			float m04, float m05, float m06, float m07,
+			float m08, float m09, float m10, float m11,
+			float m12, float m13, float m14, float m15) 
+	{
+		m[0] = m00;  m[1] = m01;  m[2] = m02;  m[3] = m03;
+		m[4] = m04;  m[5] = m05;  m[6] = m06;  m[7] = m07;
+		m[8] = m08;  m[9] = m09;  m[10] = m10; m[11] = m11;
+		m[12] = m12; m[13] = m13; m[14] = m14; m[15] = m15;
 	}
 
 	void loadIdentity() {
@@ -35,40 +42,27 @@ struct Matrix4
 		load(i);
 	}
 
-	Matrix4 loadZeroMatrix()
+	//Renvoie une matrice zero
+	static Matrix4 loadZeroMatrix()
 	{
-		static const float i[] = {	0.f, 0.f, 0.f, 0.f,
-									0.f, 0.f, 0.f, 0.f,
-									0.f, 0.f, 0.f, 0.f,
-									0.f, 0.f, 0.f, 0.f };
-		return Matrix4(i);
+		return Matrix4 (0.f, 0.f, 0.f, 0.f,
+						0.f, 0.f, 0.f, 0.f,
+						0.f, 0.f, 0.f, 0.f,
+						0.f, 0.f, 0.f, 0.f );
 	}
 
 	// permet de charger le contenu de la matrice avec un tableau de 16 float (4 colonnes * 4 lignes)
 	void load(const float* data) {
 		memcpy(m, data, sizeof(float) * 16);
 	}
-
-	// coder ici les autres fonctions 
-	// (translate, scale, rotate, perspective,
-	// etc...)
 	
-	Matrix4 operator*(Matrix4 b)
-	{
-		Matrix4 result = loadZeroMatrix();
+	Matrix4 operator*(Matrix4 b);
 
-		for (size_t i = 0; i < 4; ++i)
-		{
-			for (size_t j = 0; j < 4; ++j)
-			{
-				for (size_t k = 0; k < 4; ++k) {
-					result.m[i + 4 * j] += m[i + 4 * k] * b.m[k + 4 * j];
-				}
-			}
-		}
+	//Calcule la matrice de vue
+	static Matrix4 lookAt(Vec3 eye, Vec3 center, Vec3 up);
 
-		return result;
-	}
+	//Calcule la matrice de projection
+	static Matrix4 perspective(float fov, float aspectRatio, float nearClipPlane, float farClipPlane);
 };
 
 #endif  // MATRIX4_H 
